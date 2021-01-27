56f7b2389ddb67fc316c684e8a795bd2a0e8f7a9
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2021-01-27 18:50:46 +0100

    Create function to search for special options

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Gaining access to all elements on the website
+
 const analysisContent = document.querySelector('.analysis__content');
 const contentWrapper = document.querySelector('.analysis__text form');
 const phrase = document.querySelector('.analysis__phrase');
@@ -14,7 +16,8 @@ const message = document.querySelector('.analysis__message');
 const results = document.querySelector('.analysis__results');
 const information = document.querySelector('.analysis__info');
 const closeLink = document.querySelector('.analysis__link');
-const punctuationMarks = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", "<", ">", "?", "/", ",", ".", "'", ];
+
+// Declaration of the function showing messages to the user
 
 const showMessage = function (content) {
     message.classList.remove("visibility");
@@ -24,28 +27,78 @@ const showMessage = function (content) {
     })
 }
 
-analysisStart.addEventListener("click", function () {
-    if (analysisContent.value != "" && (phrase.value != "" || phraseLength.value != "0" || specialOptions.value != "0")) {
-        contentWrapper.style.height = "48%";
-        results.classList.remove("visibility");
-        const resultContent = analysisContent.value.split(" ");
-        for (let singleWord of resultContent) {
-            const wordWrapper = document.createElement('span');
-            wordWrapper.textContent = singleWord + " ";
-            if (singleWord == phrase.value || singleWord.length == phraseLength.value) {
-                wordWrapper.style.color = "red";
-            }
-            results.appendChild(wordWrapper);
-        }
+// Declaration of a function that gives styles to the found results
 
-
-    } else if (analysisContent.value == "" && phrase.value == "" && phraseLength.value == "0" && specialOptions.value == "0") {
-        showMessage("Wprowadź tekst do analizy. Następnie użyj przynajmniej jedną z trzech podstawowych opcji");
-    } else if (analysisContent.value != "" && phrase.value == "" && phraseLength.value == "0" && specialOptions.value == "0") {
-        showMessage("Użyj przynajmniej jedną z trzech podstawowych opcji");
+const addStyles = function (wrapper) {
+    if (fontsColor.value != "0") {
+        wrapper.style.color = fontsColor.value;
     }
+    if (fontsBold.checked) {
+        wrapper.style.fontWeight = "bold";
+    }
+    if (fontsUnderscore.checked) {
+        wrapper.style.textDecoration = "underline";
+    }
+}
 
-})
+// Declaration of the function to search for special options
+
+const search = function (pattern, flags) {
+    let regex = new RegExp(pattern, flags);
+    let fittedResults = [];
+    let indexArray = [];
+    indexArray.push(0)
+    while (result = regex.exec(analysisContent.value)) {
+        indexArray.push(result.index);
+        indexArray.push(regex.lastIndex);
+        fittedResults.push(RegExp.lastMatch);
+    }
+    indexArray.push(analysisContent.value.length);
+
+    for (let i = 1; i < indexArray.length; i++) {
+        const word = analysisContent.value.slice(indexArray[i - 1], indexArray[i]);
+        const wordWrapper = document.createElement('span');
+        wordWrapper.textContent = word;
+        for (let fittedResult of fittedResults) {
+            if (fittedResult == word) {
+                addStyles(wordWrapper);
+            }
+        }
+        results.appendChild(wordWrapper);
+    }
+}
+
+// Support for the Analyze button
+
+analysisStart.addEventListener("click", function () {
+
+    if (results.textContent == "") {
+        if (analysisContent.value != "" && (phrase.value != "" || phraseLength.value != "0" || specialOptions.value != "0")) {
+            contentWrapper.style.height = "48%";
+            results.classList.remove("visibility");
+
+            const option = specialOptions.value;
+            switch (option) {
+                case '1':
+                    search('(\\w+\\.)*\\w+@\\w+\\.\\w{2,4}', 'ig');
+                    break;
+                case '2':
+                    search('\\d{2}-\\d{3}', 'ig');
+                    break;
+                case '3':
+                    search('\\„.+\\”', 'ig');
+                    break;
+            };
+
+        } else if (analysisContent.value == "" && phrase.value == "" && phraseLength.value == "0" && specialOptions.value == "0") {
+            showMessage("Wprowadź tekst do analizy. Następnie użyj przynajmniej jednej z trzech podstawowych opcji wyszukiwania.");
+        } else if (analysisContent.value != "" && phrase.value == "" && phraseLength.value == "0" && specialOptions.value == "0") {
+            showMessage("Użyj przynajmniej jednej z trzech podstawowych opcji wyszukiwania.");
+        }
+    }
+});
+
+// Support for the Reset button
 
 analysisReset.addEventListener("click", function () {
     analysisContent.value = "";
@@ -58,18 +111,8 @@ analysisReset.addEventListener("click", function () {
     fontsUnderscore.checked = false;
     results.classList.add("visibility");
     contentWrapper.style.height = "100%";
-
-})
-
-
-
-// if (phrase.value != "0") {
-//     for (let singleWord of resultContent) {
-//         const wordWrapper = document.createElement('span');
-//         wordWrapper.textContent = singleWord + " ";
-//         if (singleWord == phrase.value) {
-//             wordWrapper.style.color = "red";
-//         }
-//         results.appendChild(wordWrapper);
-//     }
-// }
+    const words = results.querySelectorAll('span');
+    for (let i = 0; i < words.length; i++) {
+        results.removeChild(words[i]);
+    }
+});
